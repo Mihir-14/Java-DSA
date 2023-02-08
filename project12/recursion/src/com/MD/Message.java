6d87d984c36cf3f8b40722dcf6205d53e6c99b1e
Mihir Dudhatra <mihirdudhatra3906@gmail.com>
2023-02-08 19:02:43 +0530

    pattern part completed

---
+++ project12/recursion/src/com/MD/Message.java
@@ -0,0 +1,10 @@
+package com.MD;
+
+public class Message {
+    public static void main(String[] args) {
+
+    }
+    static void message(){
+        System.out.println("Hello World");
+    }
+}
---
+++ project12/recursion/src/com/MD/Numbers.java
@@ -0,0 +1,17 @@
+package com.MD;
+
+public class Numbers {
+    public static void main(String[] args) {
+//        write a function that takes a number and print it
+//        print first 5 number  1 2 3 4 5
+        print1(1);
+    }
+    static void print1(int n){
+        if(n == 5){
+            return;
+        }
+        System.out.println(n);
+        print1(n+1);
+//        If we call the function again and again you can treat it as separate call in the stack 
+    }
+}
